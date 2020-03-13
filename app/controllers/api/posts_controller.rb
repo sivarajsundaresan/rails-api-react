2b87c2ee217a59bfc08d5904031063f156db8316
sivarajsundar <sivarajsundaresan@gmail.com>
2020-03-14 00:41:07 +0530

    post curd update delete show

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,5 +1,7 @@
 class Api::PostsController < ApplicationController
 
+	before_action :set_post, only: [:show, :update, :destroy]
+
 	def index
 		@posts = Post.all
 		render json: @posts
@@ -16,16 +18,38 @@ class Api::PostsController < ApplicationController
 		end
 	end
 
-	def update
+	def show
+		if @post
+			render json: @post, status: :ok
+		else
+			render json: { status: false }, status: :unauthorized
+		end
+	end
 
+	def update
+		params["post"] = {name: params["name"], description: params["description"]}
+		if @post.update(post_params)
+			render json: { status: true }, status: :ok
+		else
+			render json: { status: false }, status: :ok
+		end
 	end
 
 	def destroy
+		if @post.destroy
+			render json: { status: true }, status: :ok
+		else
+			render json: { status: false }, status: :ok
+		end
 	end
 
 	private
 
 	def post_params
 		params.require(:post).permit(:name, :description)
+	end
+
+	def set_post
+		@post = Post.find_by(id: params[:id])
 	end
 end
