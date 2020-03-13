8b8e1ac985d0cedcc7307ba1adb701c4388adb61
sivarajsundar <sivarajsundaresan@gmail.com>
2020-03-13 20:56:57 +0530

    post create api added

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -5,4 +5,20 @@ class Api::PostsController < ApplicationController
 		render json: @posts
 	end
 	
+	def create
+		@post = Post.new(params)
+
+		if @post.save
+			render json: { status: true }, status: :ok
+		else
+			render json: { status: false }, status: :error
+		end
+	end
+
+	def update
+
+	end
+
+	def destroy
+	end
 end
