3eb7e03fa800bb68c97cdd119c4357a5502badaf
sivarajsundar <sivarajsundaresan@gmail.com>
2020-03-17 23:44:55 +0530

    changes

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,12 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
 	allow do
-		origins "http://localhost:3000"
+		origins "https://rails-backend-api-react.herokuapp.com/"
+		resource "*", headers: :any, methods: [:get, :post, :put, :patch, :options, :head],
+		credentials: true
+	end
+
+	allow do
+		origins "http://localhost:3000/"
 		resource "*", headers: :any, methods: [:get, :post, :put, :patch, :options, :head],
 		credentials: true
 	end
