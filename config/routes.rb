f2b193d4c88fb96af88e88ae9bb0a28ab6b0153b
sivarajsundar <sivarajsundaresan@gmail.com>
2020-03-17 21:06:44 +0530

    routes changes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   	resources :posts
   	post 'authenticate', to: 'authentication#authenticate'
   	# resources :sessions, only: [:create]
-  	# resources :registrations, only: [:create]
+  	resources :registrations, only: [:create]
   	# delete :logout, to: "sessions#logout"
   	# get :logged_in, to: "sessions#logged_in"
   end
