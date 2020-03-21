bd4d7f295ecece1343323c1ede15c93ea3d97a4a
siva.sundar <siva.sundar@philips.com>
2020-03-21 22:49:10 +0530

    registration user changes

--- app/controllers/api/registrations_controller.rb
+++ app/controllers/api/registrations_controller.rb
@@ -4,8 +4,7 @@ class Api::RegistrationsController < ApplicationController
 	def create
 		p params
 		user = User.create(email: params['user']['email'], password: params['user']['password'], password_confirmation: params['user']['password_confirmation'])
-		if user
-			# session[:user_id] = user.id
+		if user.id.present?
 			render json: {
 				status: :created,
 				user: user
